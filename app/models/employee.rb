cd972b1f936f9b8e965885acbe5b2da4dbb5d0a0
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-07-04 10:40:11 +0430

    add signing in tools in employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,19 @@
+# encoding: utf-8
+
 class Employee < ActiveRecord::Base
+  has_secure_password
+
+  after_save :regenerate_auth_token
+
+  validates :email, uniqueness: true
+
+  def regenerate_auth_token
+    update_columns(auth_token: generate_token)
+  end
+
+  def generate_token
+    salt = ";oafoijoajfosj owoeijfos ojfoajsofjoeifsoajol$#$$^%%*&}"
+    text = salt + Time.new.to_s
+    Digest::MD5.hexdigest(text)
+  end
 end
