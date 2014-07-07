3c276d87fb341f6b8cb87df7f9e6beb4ed0f25d8
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-07-07 22:33:24 +0430

    add backup and restore abiliy to employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,5 +4,25 @@ class EmployeesController < ApplicationController
   
   def show
     @employee = Employee.find_by(auth_token: current_employee.auth_token)   
+  end
+
+  def backup
+    if system("mysqldump -u root banks > #{Rails.root.to_s}/db/backup.sql")
+      flash[:success] = "Successfully made a backup of your database!"
+    else
+      flash[:danger] = "Backing up proccess was unseccessful."
+    end
+
+    redirect_to current_employee
+  end
+
+  def restore
+    if system("mysql -u root banks < #{Rails.root.to_s}/db/backup.sql")
+      flash[:success] = "Backup successfully restored!"
+    else
+      flash[:danger] = "We couldn't restore database."
+    end
+    
+    redirect_to current_employee
   end  
 end
