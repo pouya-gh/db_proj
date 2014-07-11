3fcd6deb9a00c1e0b0598a49988a51802fb7c7ed
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-07-11 13:55:25 +0430

    add execution method

--- app/models/query.rb
+++ app/models/query.rb
@@ -2,4 +2,8 @@ class Query < ActiveRecord::Base
   validates :query_string, presence: true
   validates :description, presence: true
   validates :report_order, presence: true
+
+  def sql
+    ActiveRecord::Base.connection.execute(self.query_string).as_json
+  end
 end
