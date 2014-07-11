0365d143f0a5e8edaa8e1276eef0b51f13181611
pouya <pouya@pouya-VPCEH2BGG.(none)>
2014-07-11 12:25:34 +0430

    add filter for updating branch assets

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,19 @@ class Account < ActiveRecord::Base
   has_many :ac_relationships, dependent: :destroy
   has_many :customers, through: :ac_relationships 
   validates :branch_id, presence: true
+
+  after_commit :increase_branch_asset, on: [:create]
+  before_destroy :decrease_branch_asset
+
+  def increase_branch_asset
+    branch = Branch.find(self.branch_id)
+    branch.asset += self.balance
+    branch.save
+  end
+
+  def decrease_branch_asset
+    branch = Branch.find(self.branch_id)
+    branch.asset -= self.balance
+    branch.save
+  end
 end
